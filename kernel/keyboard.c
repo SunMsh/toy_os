203b62d9a84d44330cdfdb5441178c30fe39abea
SunMs <sms_fengyang@163.com>
2017-01-04 22:45:42 +0800

    Update tty function for keys with three-bytes code.

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -19,7 +19,7 @@
 
 PRIVATE KB_INPUT	kb_in;
 
-PRIVATE	int	code_with_E0 = 0;
+PRIVATE	int	code_with_E0;
 PRIVATE	int	shift_l;	/* l shift state */
 PRIVATE	int	shift_r;	/* r shift state */
 PRIVATE	int	alt_l;		/* l alt state	 */
@@ -30,6 +30,8 @@ PRIVATE	int	caps_lock;	/* Caps Lock	 */
 PRIVATE	int	num_lock;	/* Num Lock	 */
 PRIVATE	int	scroll_lock;	/* Scroll Lock	 */
 PRIVATE	int	column;
+
+PRIVATE u8	get_byte_from_kbuf();
 
 /*======================================================================*
                             keyboard_handler
@@ -73,33 +75,61 @@ PUBLIC void keyboard_read()
 {
 	u8	scan_code;
 	char	output[2];
-	int	make;	/* TRUE: make;  FALSE: break. */
+	int	make;	/* 1: make;  0: break. */
 
 	u32	key = 0;/* 用一个整型来表示一个键。比如，如果 Home 被按下，
 			 * 则 key 值将为定义在 keyboard.h 中的 'HOME'。
 			 */
 	u32*	keyrow;	/* 指向 keymap[] 的某一行 */
-	/* memset(output, 0, 2); */
 
 	if(kb_in.count > 0){
-		disable_int();
-		scan_code = *(kb_in.p_tail);
-		kb_in.p_tail++;
-		if (kb_in.p_tail == kb_in.buf + KB_IN_BYTES) {
-			kb_in.p_tail = kb_in.buf;
-		}
-		kb_in.count--;
-		enable_int();
+		code_with_E0 = 0;
+
+		scan_code = get_byte_from_kbuf();
 
 		/* 下面开始解析扫描码 */
 		if (scan_code == 0xE1) {
-			/* 暂时不做任何操作 */
+			int i;
+			u8 pausebrk_scode[] = {0xE1, 0x1D, 0x45,
+					       0xE1, 0x9D, 0xC5};
+			int is_pausebreak = 1;
+			for(i=1;i<6;i++){
+				if (get_byte_from_kbuf() != pausebrk_scode[i]) {
+					is_pausebreak = 0;
+					break;
+				}
+			}
+			if (is_pausebreak) {
+				key = PAUSEBREAK;
+			}
 		}
 		else if (scan_code == 0xE0) {
-			code_with_E0 = 1;
-		}
-		else {	/* 下面处理可打印字符 */
+			scan_code = get_byte_from_kbuf();
 
+			/* PrintScreen 被按下 */
+			if (scan_code == 0x2A) {
+				if (get_byte_from_kbuf() == 0xE0) {
+					if (get_byte_from_kbuf() == 0x37) {
+						key = PRINTSCREEN;
+						make = 1;
+					}
+				}
+			}
+			/* PrintScreen 被释放 */
+			if (scan_code == 0xB7) {
+				if (get_byte_from_kbuf() == 0xE0) {
+					if (get_byte_from_kbuf() == 0xAA) {
+						key = PRINTSCREEN;
+						make = 0;
+					}
+				}
+			}
+			/* 不是PrintScreen, 此时scan_code为0xE0紧跟的那个值. */
+			if (key == 0) {
+				code_with_E0 = 1;
+			}
+		}
+		if ((key != PAUSEBREAK) && (key != PRINTSCREEN)) {
 			/* 首先判断Make Code 还是 Break Code */
 			make = (scan_code & FLAG_BREAK ? 0 : 1);
 
@@ -144,16 +174,38 @@ PUBLIC void keyboard_read()
 				break;
 			default:
 				if (!make) {	/* 如果是 Break Code */
-					key = 0;	/* 忽略之 */
+					key = 0;/* 忽略之 */
 				}
 				break;
 			}
 
 			/* 如果 Key 不为0说明是可打印字符，否则不做处理 */
-			if(key){
+			if (key) {
 				output[0] = key;
 				disp_str(output);
 			}
 		}
 	}
 }
+
+/*======================================================================*
+			    get_byte_from_kbuf
+ *======================================================================*/
+PRIVATE u8 get_byte_from_kbuf()       /* 从键盘缓冲区中读取下一个字节 */
+{
+        u8 scan_code;
+
+        while (kb_in.count <= 0) {}   /* 等待下一个字节到来 */
+
+        disable_int();
+        scan_code = *(kb_in.p_tail);
+        kb_in.p_tail++;
+        if (kb_in.p_tail == kb_in.buf + KB_IN_BYTES) {
+                kb_in.p_tail = kb_in.buf;
+        }
+        kb_in.count--;
+        enable_int();
+
+        return scan_code;
+}
+
